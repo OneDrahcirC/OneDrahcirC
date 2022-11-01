7a66c667f232e94e0a8e4ae025409b8eaefb6310
Richard C <112592860+OneDrahcirC@users.noreply.github.com>
2022-11-01 01:39:53 -0400

    react update
    trying the flair

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,23 @@
-const menu = document.getElementsById("menu");
+import React from 'react';
+import './App.css';
+import Menu from './components/Menu';
+function App() {
+  return (
+    <div className="App">
 
-Array.from(document.getElementsByClassName("menu-item"))
-.forEach((item, index) =>{
-    item.onmouseover = () => {
-menu.dataset.activeIndex = index;
- }
-});
+<div id="menu">
+    <div id="menu-item">
+<a href="/" class="menu-item">Home</a>
+<a href="/" class="menu-item">Bio1</a>
+<a href="/" class="menu-item">Bio2</a>
+<a href="/" class="menu-item">React</a>
+    </div>
+    <div id="menu-backgroud-pattern"></div>
+<div id="menu-backgroud-image"></div>
+
+</div>
+    </div>
+  );
+}
+
+export default App;
